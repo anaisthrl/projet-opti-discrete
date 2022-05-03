a8097944fc69369cb91e0b81b308f9424415d12e
AlexandreLaval <herstalex@gmail.com>
2022-05-03 14:23:33 +0200

    Generation aléatoire (#2)

--- projet/src/main/java/Client.java
+++
@@ -1,7 +0,0 @@
-public class Client extends Node {
-    public Client(int x, int y, int q) {
-        this.posX = x;
-        this.posY = y;
-        this.poids = q;
-    }
-}
--- projet/src/main/java/Depot.java
+++
@@ -1,7 +0,0 @@
-public class Depot extends Node {
-    public Depot(int x, int y, int q) {
-        this.posX = x;
-        this.posY = y;
-        this.poids = q;
-    }
-}
