cc9f7ed6a98f6269180e51f9311fb3ca4210eadc
anaisthrl <anaisthoral@gmail.com>
2022-05-11 16:52:44 +0200

    creation de l'operateur de voisinage exchange

--- src/main/java/OperateursVoisinage.java
+++ src/main/java/OperateursVoisinage.java
@@ -24,4 +24,18 @@ public class OperateursVoisinage {
     }
 
 
+    //cette fonction échange deux clients données dans notre tournée
+    public void exchange2Clients(ArrayList<Node> list,int indexC1, int indexC2){
+        Node tempNode = list.get(indexC1);
+        list.set(indexC1,list.get(indexC2));
+        list.set(indexC2,tempNode);
+    }
+
+    //cette fonction va échanger tous les clients de notre tournée
+    public void exchangeAllClients(ArrayList<Node> list){
+        for(int i =0; i<= list.size()-1; i++){
+            if(i==list.size()-1) exchange2Clients(list,i,0);
+            else exchange2Clients(list, i,i+1);
+        }
+    }
 }
