8aafc6ee3a977c76b5341156100f4f26b845359b
anaisthrl <anaisthoral@gmail.com>
2022-05-11 16:44:12 +0200

    correction bug

--- src/main/java/OperateursVoisinage.java
+++ src/main/java/OperateursVoisinage.java
@@ -4,21 +4,21 @@ public class OperateursVoisinage {
 
     OperateursVoisinage(){}
 
-    //cette fonction échange deux clients données dans notre tournée
-    public void relocate2Clients(ArrayList<Node> list,int indexC1, int indexC2){
-        if(indexC1 != 0 || indexC2 != 0) { // on ne change pas le point d'arrivée
-            Node tempNode = list.get(indexC1);
-            list.set(indexC1, list.get(indexC2));
-            list.set(indexC2, tempNode);
-        }
-    }
-
-    //cette fonction va échanger tous les clients de notre tournée
-    public void relocateAllClients(ArrayList<Node> list){
-        for(int i =1; i<= list.size()-1; i++){
-            if(list.size() > 2) { // s'il n'y a que le point de départ et un autre point, on ne pourra pas changer
-                if (i == list.size() - 1) relocate2Clients(list, i, 1);
-                else relocate2Clients(list, i, i + 1);
+    //cette fonction va placer un client derrière un autre pour une tournée donnée
+    public void relocate2Clients(ArrayList<Node> list,int indexC1, int indexCAChanger){
+        if(indexCAChanger != 0 && indexCAChanger < list.size() && indexC1 < list.size()) { // on ne change pas le point d'arrivée + vérification indexOutOfBounds
+            Node tempNode = list.get(indexC1+1);
+            list.set(indexC1+1, list.get(indexCAChanger));
+            for(int i = indexC1+2; i<list.size(); i++){
+                Node tempNode2 = list.get(i);
+                if(tempNode2 == list.get(indexC1+1)) {
+                    list.set(i,tempNode);
+                    return;
+                }
+                else{
+                    list.set(i,tempNode);
+                    tempNode = tempNode2;
+                }
             }
         }
     }
