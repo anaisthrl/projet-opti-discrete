0595775e56beb2ebc121d18cb074d7a0fea7343d
anaisthrl <anaisthoral@gmail.com>
2022-05-11 15:25:13 +0200

    specification pour un trajet avec un seul client

--- src/main/java/OperateursVoisinage.java
+++ src/main/java/OperateursVoisinage.java
@@ -6,16 +6,20 @@ public class OperateursVoisinage {
 
     //cette fonction échange deux clients données dans notre tournée
     public void relocate2Clients(ArrayList<Node> list,int indexC1, int indexC2){
-        Node tempNode = list.get(indexC1);
-        list.set(indexC1,list.get(indexC2));
-        list.set(indexC2,tempNode);
+        if(indexC1 != 0 || indexC2 != 0) { // on ne change pas le point d'arrivée
+            Node tempNode = list.get(indexC1);
+            list.set(indexC1, list.get(indexC2));
+            list.set(indexC2, tempNode);
+        }
     }
 
     //cette fonction va échanger tous les clients de notre tournée
     public void relocateAllClients(ArrayList<Node> list){
-        for(int i =0; i<= list.size()-1; i++){
-            if(i==list.size()-1) relocate2Clients(list,i,0);
-            else relocate2Clients(list, i,i+1);
+        for(int i =1; i<= list.size()-1; i++){
+            if(list.size() > 2) { // s'il n'y a que le point de départ et un autre point, on ne pourra pas changer
+                if (i == list.size() - 1) relocate2Clients(list, i, 1);
+                else relocate2Clients(list, i, i + 1);
+            }
         }
     }
 
